6a71087d8f530ffcf277a1dc801c706537578c5b
rohitnakashe <52915840+rohitnakashe@users.noreply.github.com>
2025-02-19 11:08:18 -0800

    Create tb.sv

---
+++ tb.sv
@@ -0,0 +1,25 @@
+module tb;
+    
+  fifo_if fif();
+  FIFO dut (fif.clock, fif.rst, fif.wr, fif.rd, fif.data_in, fif.data_out, fif.empty, fif.full);
+    
+  initial begin
+    fif.clock <= 0;
+  end
+    
+  always #10 fif.clock <= ~fif.clock;
+    
+  environment env;
+    
+  initial begin
+    env = new(fif);
+    env.gen.count = 20;
+    env.run();
+  end
+    
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars;
+  end
+   
+endmodule
